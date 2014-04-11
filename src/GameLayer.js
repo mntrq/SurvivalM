676d5b89c651ff95534aeae4e0629552f1be37d2
M <Martin@kuwinwpa-136-150.kuwin.ku.ac.th>
2014-04-11 11:42:18 +0700

    separate init in gameLayer

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -3,32 +3,50 @@ var GameLayer = cc.LayerColor.extend({
         this._super(new cc.Color4B(127, 127, 127, 255));
         this.setPosition(new cc.Point(0,0));
 
-        this.bg = new Background();
-        this.bg.setPosition(cc.p(screenWidth/2, screenHeight/2));
-        this.addChild(this.bg);
-
-        this.player = new Player();
-        this.player.setPosition(cc.p(screenWidth - 450, screenHeight - 350));
-        this.addChild(this.player);
+        this.initBG();
+        this.initPlayer();
+        this.initBall();
         
         this.setKeyboardEnabled( true );
         this.startSpeed = 1.5;
         //this.schedule(this.updateStartBall,1.3,Infinity,0);
-
-        this.ball = new Array();
-        this.updateStartBall();
-        this.scheduleUpdate();
+        // this.scheduleUpdate();
         this.playerDelayDead = 0;
         this.isDead = false;
+        this.life();
+        //this.createLifeCountingLabel();
 
+        this.scheduleUpdate();
+
+        return true;
+    },
+
+    initBG: function(){
+        this.bg = new Background();
+        this.bg.setPosition(cc.p(screenWidth/2, screenHeight/2));
+        this.addChild(this.bg);
+    },
+
+    initPlayer: function(){
+        this.player = new Player();
+        this.player.setPosition(cc.p(screenWidth - 450, screenHeight - 350));
+        this.addChild(this.player);
+    },
+
+    initBall: function(){
+        this.ball = new Array();
+        this.updateStartBall();
+    },
+
+    life: function(){
+        this.numLife = 5;
         this.life = cc.Sprite.create('images/life.png');
         this.life.setPosition(cc.p(730,560));
         this.addChild(this.life);
-        this.lifeLabel = cc.LabelTTF.create('x 5','Arial',35);
-        this.lifeLabel.setPosition(cc.p(770,550));
-        this.addChild(this.lifeLabel);
 
-        return true;
+        this.lifeCount = cc.LabelTTF.create('x ' + this.numLife,'Arial',35);
+        this.lifeCount.setPosition(cc.p(770,550));
+        this.addChild(this.lifeCount);
     },
 
     updateStartBall: function() {
@@ -41,6 +59,7 @@ var GameLayer = cc.LayerColor.extend({
 
     update: function(dt){
         if(this.isDead){
+            this.numLife--;
             this.playerDelayDead++;
         }
 
@@ -87,9 +106,17 @@ var GameLayer = cc.LayerColor.extend({
         this.player.runAction(this.player.createStandAction());
     },
 
-    lifeCounting: function(){
+    // createLifeCountingLabel: function(){
+    //     this.numLife = 5;
+    //     this.life = cc.Sprite.create('images/life.png');
+    //     this.life.setPosition(cc.p(730,560));
+    //     this.addChild(this.life);
 
-    },
+    //     this.lifeCount = cc.LabelTTF.create('x ' + this.numLife,'Arial',35);
+    //     this.lifeCount.setPosition(cc.p(770,550));
+    //     this.addChild(this.lifeCount);
+
+    // }
 
 });
 
